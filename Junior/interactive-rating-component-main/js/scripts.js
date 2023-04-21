822fd562b46a05b686bc8e47cfab3bddcab36547
Brandon <name.brandon.aguero@gmail.com>
2023-04-21 14:45:09 -0500

    Interactive rating component main, este proyecto fue terminado

--- Junior/interactive-rating-component-main/js/scripts.js
+++ Junior/interactive-rating-component-main/js/scripts.js
@@ -1,9 +1,61 @@
 const circles = document.querySelectorAll(".ul__li");
+const primaryButton = document.querySelector(".main__article--button");
+const firstCard = document.querySelector(".main__article--first");
+const secondCard = document.querySelector(".main__article--second");
+const mainArticleSpan = document.querySelector(".main__article--span");
 
-for (const circle of circles) {
-    circle.addEventListener("click", choiceCircle);
-}
+// circles.forEach((li) => {
+//     li.addEventListener("click", (e) => {
+// /*         circles.forEach((li) => {
+//             if (!li.classList.contains("activate")) {
+//                 li.classList.remove("activate");
+//             }
+//         }) */ // Este selecciona a más de uno y puede eliminarlo también
+//         let counter = 0;
+//         circles.forEach((li) => {
+//             if (li.classList.contains("activate")) {
+//                 li.classList.remove("activate");
+//             }
+//         })   // Este solo selecciona a uno pero no puede eliminarlo.
+//         e.target.classList.toggle("activate");
+//         counter++;
+//     })
+// })
 
-function choiceCircle() {
-    
-}
+/* primaryButton.addEventListener("click", () => {
+    firstCard.classList.add("inactivate");
+    secondCard.classList.remove("inactivate")
+}); */
+
+circles.forEach((li) => {
+    li.addEventListener("click", clickCircleOpinion);
+})
+
+primaryButton.addEventListener("click", clickPrimaryButton);
+
+function clickCircleOpinion(e) {
+    circles.forEach((li) => {
+        if (li.classList.contains("activate")) {
+            li.classList.remove("activate");
+        };
+    });
+    e.target.classList.toggle("activate");
+};
+
+function clickPrimaryButton() {
+    let counter = 0;
+    let textContent;
+    circles.forEach((li) => {
+        if (li.classList.contains("activate")) {
+            textContent = li.textContent;
+            counter++;
+        };
+    })
+    if (counter === 1) {
+        firstCard.classList.add("inactivate");
+        secondCard.classList.remove("inactivate");
+    };
+
+    mainArticleSpan.innerText = `You selected ${textContent} out of 5`;
+};
+
